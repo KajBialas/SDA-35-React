665ceec247f772e62be58a77cc3007e122a020c1
Kaj Białas <kaj.bialas@dev-industry.com>
2024-06-09 11:41:28 +0200

    First component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import Header from './components/header/Header';
+
 function App() {
 
   return (
     <>
+      <Header />
       <div>
-        Moje pierwsza aplikacja!
+        Zawartość komponentu APP
       </div>
     </>
   )
 }
 
 export default App
+
+
+// Zadanie 4
+// Utwórz komponenty Header, TextSection oraz Footer
+// Komponenty powinny zawierać statyczną treść
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+// function Header() {
+//     return (
+//         <header>
+//             Moja pierwsza aplikacja
+//         </header>
+//     )
+// }
+
+const Header = () => {
+    return (
+        <header>
+            Moja pierwsza aplikacja
+        </header>
+    )
+}
+
+export default Header;
