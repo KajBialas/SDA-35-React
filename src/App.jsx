7969f492cc9f719959ab79d329fa3c76bc4cd5d8
Kaj Białas <kaj.bialas@dev-industry.com>
2024-06-09 14:19:12 +0200

    Props for text section

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <>
       <Header />
-      <TextSection />
-      <TextSection />
+      <TextSection content="During development, Vite sets esnext as the transform target, because we assume a modern browser is used and it supports all of the latest JavaScript and CSS features. This prevents syntax lowering, letting Vite serve modules as close as possible to the original source code." />
+      <TextSection content="For the production build, by default Vite targets browsers that support native ES Modules, native ESM dynamic import, and import.meta. Legacy browsers can be supported via the official @vitejs/plugin-legacy. See the Building for Production section for more details." />
       <Footer />
     </>
   )
@@ -16,3 +16,8 @@ function App() {
 
 export default App
 
+
+
+// Zadanie 7
+// Zmodyfikuj komponenty Header, footer oraz textSection - tak zeby wyświetlały dane dostarczone
+// w mechanizmie props
--- src/components/textSection/TextSection.jsx
+++ src/components/textSection/TextSection.jsx
@@ -1,14 +1,29 @@
 import styles from "./textSection.module.css";
 
-const TextSection = () => {
+const TextSection = ({ content }) => {
+    
     return (
         <section className={styles.textSectionContainer}>
             <p>
-                During development, Vite sets esnext as the transform target, because we assume a modern browser is used and it supports all of the latest JavaScript and CSS features. This prevents syntax lowering, letting Vite serve modules as close as possible to the original source code.
-                For the production build, by default Vite targets browsers that support native ES Modules, native ESM dynamic import, and import.meta. Legacy browsers can be supported via the official @vitejs/plugin-legacy. See the Building for Production section for more details.
+               {content} 
             </p>
         </section>
     )
 }
 
 export default TextSection;
+
+// import styles from "./textSection.module.css";
+
+// const TextSection = (props) => {
+    
+//     return (
+//         <section className={styles.textSectionContainer}>
+//             <p>
+//                {props.content} 
+//             </p>
+//         </section>
+//     )
+// }
+
+// export default TextSection;
