90155af863d30732cb92f7e82be61acec2844b97
Kaj Białas <kaj.bialas@dev-industry.com>
2024-06-15 11:54:40 +0200

    Form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,24 +1,80 @@
 import { useState } from "react";
 
 const Form = () => {
-    const [inputName, setInputName] = useState('');
+    const [ inputName, setInputName ] = useState('');
+    const [ inputSurname, setInputSurname ] = useState('');
+    const [ inputAdress, setInputAdress ] = useState('');
+    const [ inputEmail, setInputEmail ] = useState('');
+    const [ users, setUsers ] = useState([]);
+
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(inputName);
+
+        setUsers([{
+            name: inputName,
+            surName: inputSurname,
+            adress: inputAdress,
+            email: inputEmail
+        }, ...users]);
+
+
+        setInputName('');
+        setInputSurname('');
+        setInputEmail('');
+        setInputAdress('');
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>Name:
-                <input 
-                    type="text" 
-                    value={inputName} 
-                    onChange={(event) => setInputName(event.target.value)} 
-                />
-            </label>
-            <button type="submit">Wyślij</button>
-        </form>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Name:
+                        <input 
+                            type="text" 
+                            value={inputName} 
+                            onChange={(event) => setInputName(event.target.value)} 
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>Surname:
+                        <input 
+                            type="text" 
+                            value={inputSurname} 
+                            onChange={(event) => setInputSurname(event.target.value)} 
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>Adress:
+                        <input 
+                            type="text" 
+                            value={inputAdress} 
+                            onChange={(event) => setInputAdress(event.target.value)} 
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>Email:
+                        <input 
+                            type="text" 
+                            value={inputEmail} 
+                            onChange={(event) => setInputEmail(event.target.value)} 
+                        />
+                    </label>
+                </div>
+                
+                <button type="submit">Wyślij</button>
+            </form>
+            <div>
+                <h1>
+                    Uzytkownicy:
+                </h1>
+                { users.map(element => <div>{element.name}</div>)}
+            </div>
+        </div>
+        
     )
 }
 
