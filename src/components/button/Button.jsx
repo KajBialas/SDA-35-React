11e6cd66a79e5b1fccad1cfe7bebfd52c3b1907a
Kaj Białas <kaj.bialas@dev-industry.com>
2024-06-15 10:22:37 +0200

    Button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -11,9 +11,3 @@ const Button = ({text, handleClick, type}) => {
 }
 
 export default Button;
-
-
-const x = 5;
-const string = 'Mam 5 lat';
-const string2 = 'Mam ' + x + ' lat';
-const string3 = `Mam ${x} lat`
