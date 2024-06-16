53937183216fec341f24c0e62799d426450c9ab3
Kaj Białas <kaj.bialas@dev-industry.com>
2024-06-16 12:06:14 +0200

    Add loader and error

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -2,11 +2,30 @@ import { useState, useEffect } from 'react';
 
 const Blog = () => {
     const [ blogData, setBlogData] = useState([]);
+    const [ isLoading, setIsLoading] = useState(false);
+    const [ isError, setIsError] = useState(false);
 
     useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(data => setBlogData(data));
+        setIsLoading(true);
+        fetch('https://jsonplaceholder.typicode.com/posts22')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Wrong status');
+                }
+                return response.json()
+            })
+            .then(data => {
+                setTimeout(() => {
+                    setBlogData(data);
+                    setIsLoading(false);
+                }, 3000);
+                
+            })
+            .catch( error => {
+                setIsError(true);
+                setIsLoading(false);
+
+            });
     }, []);
 
     const renderBlogPosts = () => blogData.map(blogPost => (
@@ -19,9 +38,18 @@ const Blog = () => {
     return (
         <>
             <h1>Blog</h1>
+            {isLoading && <div>Ładowanie...</div>}
+            {isError && <div>Błąd ładowania danych</div>}
             {renderBlogPosts()}
         </>
     )
 }
 
 export default Blog;
+
+// Zadanie 3
+// Zaimplementuj obsluge błędów oraz statusu ładowania
+
+// Zadanie 3 dodatkowe
+// Zaimplementuj graficzną wersję loadera
+// Wyświetl informacje o błędzie, zwróconą z kodu
