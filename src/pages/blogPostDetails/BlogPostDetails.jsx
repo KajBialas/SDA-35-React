9323a57cd7c2cde7cb3333b76605fa74fac74133
Kaj Białas <kaj.bialas@dev-industry.com>
2024-06-16 13:49:30 +0200

    Blog post details

--- src/pages/blogPostDetails/BlogPostDetails.jsx
+++ src/pages/blogPostDetails/BlogPostDetails.jsx
@@ -1,14 +1,44 @@
+import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
+
 const BlogPostDetails = () => {
     const { postId } = useParams();
+    const [ postDeatails, setPostDetails ] = useState({});
+    const [ isLoading, setIsLoading ] = useState(false);
+    const [ error, setError ] = useState(null);
 
-    // https://jsonplaceholder.typicode.com/posts/5(postID)
+    useEffect( () => {
+        setIsLoading(true);
+        setError(null);
 
-    // useEffect z fetch ()
+        fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Błąd pobierania danych');
+                }
+                return response.json();
+            })
+            .then(data => {
+                setPostDetails(data);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                setError(error);
+                setIsLoading(false);
+            })
+    }, [postId]);
+
+    
 
     return(
         <>
             <h1>Szczegóły postu: {postId} </h1>
+            {isLoading && <div>Ładowanie danych</div>}
+            {error && <div>Błąd: {error.message}</div>}
+            <div>
+                <h3>{postDeatails.title}</h3>
+                <p>{postDeatails.body}</p>
+            </div>
         </>
     )
 }
